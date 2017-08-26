f8aab5c7ff9721900158bf2aba07c9f117448ed1
PriscaDQ <priscadq@gmail.com>
2017-08-26 11:41:35 -0300

    submit02

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,4 +1,17 @@
-alert ("hola mundo")
+//alert ("hola mundo")
+
+var local01 = "ejemplo variable Local 01";
 function mifuncion() {
-    
+ 
+ alert("mi local var es: " + local01);
+
 }
+
+function mifuncion2() {
+    alert("Desde Funcion2:" + local01);
+}
+
+
+//function sumaloca(var1, var2) {
+//   alert("El resultado es:" + (var1 + var2));
+//}
